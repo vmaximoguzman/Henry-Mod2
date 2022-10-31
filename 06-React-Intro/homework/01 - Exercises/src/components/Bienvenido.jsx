ac20c4f9b0f972053fa18c3e8a33933f0dad75cf
Maximo Guzman <vmaximoguzman@gmail.com>
2022-10-31 17:28:24 -0300

    HW06-01

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Botones from "./Botones.jsx";
 
-const studentName = "";
+const studentName = "Máximo";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
-export default function Bienvenido() {
+export default function Bienvenido(alerts) {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((tech, index) => (
+          <li key={index}>{tech}</li>
+        ))}
+      </ul>
+
+      <Botones props={alerts} />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Botones extends React.Component {
+  render(props) {
+    return (
+      <div>
+        <button onClick={() => alert(this.props.alerts.m1)}>Módulo 1</button>
+        <button onClick={() => alert(this.props.alerts.m2)}>Módulo 2</button>
+      </div>
+    );
+  }
+}
+
+export default Botones;
