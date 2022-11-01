747b24e7fbbf87a007d2b72c321661b2b6c00fb1
Maximo Guzman <vmaximoguzman@gmail.com>
2022-11-01 06:50:33 -0300

    HW06-02-01

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,6 +1,18 @@
-import Card from './Card';
+import Card from "./Card.jsx";
 
 export default function Cards(props) {
-   const { characters } = props;
-   return <div></div>;
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((prop) => (
+        <Card
+          name={prop.name}
+          species={prop.species}
+          gender={prop.gender}
+          image={prop.image}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
+        />
+      ))}
+    </div>
+  );
 }
--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 export default function SearchBar(props) {
-   return (
-      <div>
-         {/* <input type='search' />
-      <button onClick={}>Agregar</button> */}
-      </div>
-   );
+  return (
+    <div>
+      <input type="search" />
+      <button onClick={props.onSearch}>Agregar</button>
+    </div>
+  );
 }
