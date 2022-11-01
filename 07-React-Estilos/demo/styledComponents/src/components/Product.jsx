36c54006b5c35e50c55c9a1dcfcc1c0cf07600a8
Maximo Guzman <vmaximoguzman@gmail.com>
2022-11-01 09:15:21 -0300

    Demo styledComponents

--- 07-React-Estilos/demo/styledComponents/src/components/Product.jsx
+++ 07-React-Estilos/demo/styledComponents/src/components/Product.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components"; //styled-components lo tenemos que agregar (no viene con react) que se puede ver en el package.json. Podemos buscar en internet cómo agregarlo.
 
-const Producto = styled.div`
-  color: salmon;
-  font-size: 20px;
-  & h3 {
-    background-color: SpringGreen; 
-  }
-`;
-
-const Titulo = styled.h3`
-  font-size: 30px;
+const StyledDiv = styled.div`
+  border: 1px solid black;
+  background-color: salmon;
+  width: 300px;
   &:hover {
-    color: grey;
+    background-color: red;
+    transition: 0.5s;
   }
+`; //Dijimos creame un componente de estilos (Styled Component). En este caso, se pueden utilizar pseudo selectores.
+//Al profe no le gusta que le quede mucho código en el componente (A pesar de que crea que igual está bueno), a mí tampoco igual :/.
+
+const OtroDiv = styled.div`
+  border: 3px solid blue;
+  background-color: grey;
+  width: 200px;
 `;
+
+const Parrafo = styled.p`
+  color: blue;
+`;
+//Estos se pueden exportar e utilizarlo en otros lados. Al igual que podemos crear un .js donde tendremos los estilos e importarlos a nuestro .jsx.
 
 function Product(props) {
   return (
-    <Producto>
-      <Titulo>{props.title}</Titulo>
-      <p>{props.price}</p>
-    </Producto>
+    <>
+      <StyledDiv>
+        <h3>{props.title}</h3>
+        <Parrafo>{props.price}</Parrafo>
+      </StyledDiv>
+      {/* //Esto sigue siendo un div, PERO con los estilos agregados. */}
+      <OtroDiv>
+        <h3>{props.title}</h3>
+        <Parrafo>{props.price}</Parrafo>
+      </OtroDiv>
+    </>
   );
 }
 
